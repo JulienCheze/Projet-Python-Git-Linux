d731c4a7a90485b4ffd6e006e11dcf1259454c04
JulienCheze <julizn.cheze@edu.devinci.fr>
2025-04-04 21:54:04 +0000

    ajout de la generationn de timestamp dans le script

--- bash_scraper/scraper.sh
+++ bash_scraper/scraper.sh
@@ -7,5 +7,9 @@ curl -s -A "Mozilla/5.0" "$URL" > page.html
 #extrait valeur num cac 40
 valeur=$(grep -i "Cac 40" page.html | grep -Eo "[0-9]+\.[0-9]+" | head -1)
 
+#crea de timestamp
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+
 #affichage valeur terminal
 echo "Val actuelle du CAC 40 : $valeur"
