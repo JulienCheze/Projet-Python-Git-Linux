d00f9ddb7de00e5b7860ba881cb4001a0778d7d0
JulienCheze <julizn.cheze@edu.devinci.fr>
2025-04-04 21:46:29 +0000

    aajout de l'extraction de la valeur du CAC40 depuisSelexium avec grep

--- bash_scraper/scraper.sh
+++ bash_scraper/scraper.sh
@@ -3,3 +3,9 @@
 # page qu'n va  scraper
 URL="https://www.selexium.com/bourse/cac-40/"
 curl -s -A "Mozilla/5.0" "$URL" > page.html
+
+#extrait valeur num cac 40
+valeur=$(grep -i "Cac 40" page.html | grep -Eo "[0-9]+\.[0-9]+" | head -1)
+
+#affichage valeur terminal
+echo "Val actuelle du CAC 40 : $valeur"
