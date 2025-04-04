3dc9fc6a0e9e0cfc7ff480165e419782725f57ae
JulienCheze <julizn.cheze@edu.devinci.fr>
2025-04-04 22:32:45 +0000

    ajout ligne de code pr lecture csv + affichage de la courbe CAC40 dans le dashboard

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,10 +1,18 @@
 import pandas as pd
 from dash import Dash, html
+import plotly.express as px
 
 #lecture du fichier csv contenant les donnees du cac40 qui nous interessent
+df = pd.read_csv("../bash_scraper/cac40.csv", names=["timestamp", "value"])
+
+#creation de notre fig avec plotly express
+fig = px.line(df, x="timestamp", y="value", title="Évolution de l'indice CAC 40")
+
+
 app = Dash(__name__)
 app.layout = html.Div([
-    html.H1("Notre suivi en temps réel de l'indice CAC 40")
+    html.H1("Notre suivi en temps réel de l'indice CAC 40"),
+dcg.Graph(id="graph-cac40",figure=fig)
 ])
 if __name__ == "__main__":
     app.run_server(debug=True)
