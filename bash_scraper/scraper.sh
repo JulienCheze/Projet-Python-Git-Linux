71f43f2c98e1ecd0d23006c0fa9d3520dbbeec3d
JulienCheze <julizn.cheze@edu.devinci.fr>
2025-04-05 00:01:53 +0000

    update ttes les 5mins

--- bash_scraper/scraper.sh
+++ bash_scraper/scraper.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# page qu'n va  scraper
+#
+echo "------ $(date '+%Y-%m-%d %H:%M:%S') ------" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
+
+# l'url cible
 URL="https://www.selexium.com/bourse/cac-40/"
-curl -s -A "Mozilla/5.0" "$URL" > page.html
+curl -s -A "Mozilla/5.0" "$URL" -o /home/juliencheze/projet-dashboard/bash_scraper/page.html
 
-#extrait valeur num cac 40
-valeur=$(grep -i "Cac 40" page.html | grep -Eo "[0-9]+\.[0-9]+" | head -1)
+#extraire valeur
+ligne=$(grep -i "Cac 40" /home/juliencheze/projet-dashboard/bash_scraper/page.html)
+echo "Ligne trouvée : $ligne" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
 
-#crea de timestamp
+valeur=$(echo "$ligne" | grep -Eo "[0-9]+\.[0-9]+" | head -1)
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
+#on check si valeur vide
+if [[ -n "$valeur" ]]; then
+    echo "$timestamp,$valeur" >> /home/juliencheze/projet-dashboard/bash_scraper/cac40.csv
+    echo "Ajouté au CSV : $timestamp,$valeur" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
+else
+    echo "⚠️ Aucune valeur extraite — format peut-être modifié" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
+fi
 
-#affichage valeur terminal
-echo "Val actuelle du CAC 40 : $valeur"
-#enregistrement dans un csv
-
-echo "$timestamp,$valeur" >> cac40.csv
--- dash_app/app.py
+++ dash_app/app.py
@@ -1,18 +1,37 @@
 import pandas as pd
-from dash import Dash, html
+from dash import Dash, html, dcc
 import plotly.express as px
+from dash.dependencies import Input, Output
 
 #lecture du fichier csv contenant les donnees du cac40 qui nous interessent
-df = pd.read_csv("../bash_scraper/cac40.csv", names=["timestamp", "value"])
+def read_data():
+    return pd.read_csv("../bash_scraper/cac40.csv", names=["timestamp", "value"])
 
 #creation de notre fig avec plotly express
+df = read_data()
 fig = px.line(df, x="timestamp", y="value", title="Évolution de l'indice CAC 40")
-
 
 app = Dash(__name__)
 app.layout = html.Div([
-    html.H1("Notre suivi en temps réel de l'indice CAC 40"),
-dcg.Graph(id="graph-cac40",figure=fig)
+    html.H1("notre suivi en temps réel de l'indice CAC 40"),
+    dcc.Graph(id="graph-cac40", figure=fig),
+    dcc.Interval(
+        id="interval-update",
+        interval=5*60*1000,  # Maj ttess les 5 minutes en millisecondes
+        n_intervals=0  # 
+    )
 ])
+
+app.callback(
+    Output("graph-cac40", "figure"),
+    Input("interval-update", "n_intervals")
+)
+
+def update_graph(n):
+    df = read_data()  # relire les données à chaque intervalle
+    fig = px.line(df, x="timestamp", y="value", title="Évolution du CAC 40")
+    return fig
+
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8050)
+
