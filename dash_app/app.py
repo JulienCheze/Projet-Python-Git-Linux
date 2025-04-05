cba932e0a9f81625df38bc055cf24ac6120c19bf
JulienCheze <julien.cheze@edu.devinci.fr>
2025-04-05 10:15:11 +0000

    modif py  pour afficher l'évolution du CAC 40

--- dash_app/app.py
+++ dash_app/app.py
@@ -3,11 +3,11 @@ from dash import Dash, html, dcc
 import plotly.express as px
 from dash.dependencies import Input, Output
 
-#lecture du fichier csv contenant les donnees du cac40 qui nous interessent
+##lecture du fichier csv contenant les donnees du cac40 qui nous interessent
 def read_data():
     return pd.read_csv("../bash_scraper/cac40.csv", names=["timestamp", "value"])
 
-#creation de notre fig avec plotly express
+##creation de notre fig avec plotly express
 df = read_data()
 fig = px.line(df, x="timestamp", y="value", title="Évolution de l'indice CAC 40")
 
@@ -18,15 +18,15 @@ app.layout = html.Div([
     dcc.Interval(
         id="interval-update",
         interval=5*60*1000,  # Maj ttess les 5 minutes en millisecondes
-        n_intervals=0  # 
+        n_intervals=0  # Initialisation
     )
 ])
 
-app.callback(
+
+@app.callback(
     Output("graph-cac40", "figure"),
     Input("interval-update", "n_intervals")
 )
-
 def update_graph(n):
     df = read_data()  # relire les données à chaque intervalle
     fig = px.line(df, x="timestamp", y="value", title="Évolution du CAC 40")
