4fa6945e526bfecf20125c4a2d658ecfaac6db2e
JulienCheze <julien.cheze@edu.devinci.fr>
2025-04-05 09:46:40 +0000

    Maj scraper qui fonctionnait pas

--- bash_scraper/scraper.sh
+++ bash_scraper/scraper.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-#
-echo "------ $(date '+%Y-%m-%d %H:%M:%S') ------" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
-
-# l'url cible
+# page qu'on va scraper
 URL="https://www.selexium.com/bourse/cac-40/"
-curl -s -A "Mozilla/5.0" "$URL" -o /home/juliencheze/projet-dashboard/bash_scraper/page.html
+curl -s -A "Mozilla/5.0" "$URL" > /home/juliencheze/Projet-Python-Git-Linux/bash_scraper/page.html
 
-#extraire valeur
-ligne=$(grep -i "Cac 40" /home/juliencheze/projet-dashboard/bash_scraper/page.html)
-echo "Ligne trouvée : $ligne" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
+# extrait valeur num cac 40
+valeur=$(grep -oP '(?<=<span class="mx-lg-2">)[0-9]+\.[0-9]+' /home/juliencheze/Projet-Python-Git-Linux/bash_scraper/page.html | head -1)
 
-valeur=$(echo "$ligne" | grep -Eo "[0-9]+\.[0-9]+" | head -1)
+# crea timestamp
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
-#on check si valeur vide
-if [[ -n "$valeur" ]]; then
-    echo "$timestamp,$valeur" >> /home/juliencheze/projet-dashboard/bash_scraper/cac40.csv
-    echo "Ajouté au CSV : $timestamp,$valeur" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
-else
-    echo "⚠️ Aucune valeur extraite — format peut-être modifié" >> /home/juliencheze/projet-dashboard/bash_scraper/cron.log
-fi
+# affichage val terminal
+echo "Val actuelle du CAC 40 : $valeur"
+
+# enregistrement fichier CSV
+echo "$timestamp,$valeur" >> /home/juliencheze/Projet-Python-Git-Linux/bash_scraper/cac40.csv
 
